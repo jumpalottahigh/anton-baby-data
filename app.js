10a471448e223b690b1088274ae26b76a0c1236c
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-02-24 21:43:22 +0200

    set up redis

--- app.js
+++ app.js
@@ -1,21 +1,39 @@
-(function() {
-  // var redis = require("redis").createClient();
+var express = require('express');
+var app = express();
 
-  if (process.env.REDISTOGO_URL) {
-    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var redis = require("redis").createClient(rtg.port, rtg.hostname);
-    redis.auth(rtg.auth.split(":")[1]);
-  }
+//REDIS SET UP
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+  var redis = require("redis").createClient();
+}
 
-  //  else {
-  //   var redis = require("redis").createClient();
-  // }
+//REDIS TEST
+REDIS.set("answer", 42);
+REDIS.get("answer");
+var test = REDIS.get("answer");
+console.log(test);
 
-  REDIS.set("answer", 42);
-  REDIS.get("answer");
 
-  var test = REDIS.get("answer");
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-  console.log(test);
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-})();
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
