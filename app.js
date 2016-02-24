1ecd5c04417e20fc35b8c4e7c782cd85b3a771ce
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-02-24 21:19:09 +0200

    add redis hook

---
+++ app.js
@@ -0,0 +1,9 @@
+var redis = require("redis").createClient();
+
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var redis = require("redis").createClient(rtg.port, rtg.hostname);
+  redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
