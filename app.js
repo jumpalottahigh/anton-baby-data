75929c3b5b27424d406aac7482808034cde38851
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-02-24 21:28:28 +0200

    connecting redis

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var redis = require("redis").createClient();
+(function() {
+  // var redis = require("redis").createClient();
 
-if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-  var redis = require("redis").createClient(rtg.port, rtg.hostname);
-  redis.auth(rtg.auth.split(":")[1]);
-} else {
-    var redis = require("redis").createClient();
-}
+  if (process.env.REDISTOGO_URL) {
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+  }
+
+  //  else {
+  //   var redis = require("redis").createClient();
+  // }
+
+  REDIS.set("answer", 42);
+  REDIS.get("answer");
+
+  var test = REDIS.get("answer");
+
+  console.log(test);
+
+})();
